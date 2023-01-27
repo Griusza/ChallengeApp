9e6717ad8cfbdfe45577d49714abfb3e73c47aa2
Griusza <andrzejstef@gmail.com>
2023-01-27 16:48:06 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Witaj Świecie!");
+﻿string name = "Andrzej";
+char sex = 'm';
+int age = 37;
+
+if (sex == 'f')
+{
+    if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+    else if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else Console.WriteLine("Kobieta powyżej 30 lat");
+
+}
+else if (age < 18 && sex == 'm')
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else Console.WriteLine("Pełnoletni mężczyzna");
